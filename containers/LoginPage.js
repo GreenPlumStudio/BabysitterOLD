d5d2514937a5ca601aab67b902cbbb6efda229a0
nachosbeans <nachosbeans@gmail.com>
2018-09-01 23:52:51 -0700

    Fixed login/signup form input

--- containers/LoginPage.js
+++ containers/LoginPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'React';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Dimensions,  } from 'react-native';
 import { firebase } from '../utils/firebase';
 import { Constants } from 'expo';
 
@@ -21,20 +21,16 @@ export default class LoginPage extends Component {
             });
     };
 
-    handleInputChange(inputType, value) {
-        this.setState({[inputType]: value})
-    };
-
     render() {
         return (
             <View style={styles.loginPage}>
-                <Text>please dont do cocaine ey cocaine ruin your brain ey</Text>
+                <Text style={styles.logo}>Babysitter</Text>
 
                 <Text style={styles.inputLabel}>Email Address</Text>
-                <TextInput textContentType="emailAddress" value={this.state.emailAddress} onChange={event => {this.handleInputChange("emailAddress", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="emailAddress" value={this.state.emailAddress} onChangeText={text => this.setState({emailAddress: text})} />
 
                 <Text style={styles.inputLabel}>Password</Text>
-                <TextInput textContentType="password" secureTextEntry={true} value={this.state.password} onChange={event => {this.handleInputChange("password", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="password" secureTextEntry={true} value={this.state.password} onChangeText={text => this.setState({password: text})} />
 
                 <Text style={styles.errMsg}>{this.state.errMsg}</Text>
 
@@ -45,21 +41,39 @@ export default class LoginPage extends Component {
 };
 
 const styles = StyleSheet.create({
+    logo: {
+        marginTop: 30,
+        marginBottom: 30,
+        fontSize: 70,
+        fontWeight: "700",
+        color: "cornflowerblue"
+    },
+
     loginPage: {
         flex: 1,
         justifyContent: "space-around",
-        alignItems: "baseline",
+        alignItems: "center",
         marginTop: Constants.statusBarHeight,
         backgroundColor: "lightblue"
     },
 
     inputLabel: {
         fontSize: 20,
-        color: "gray"
+        color: "darkcyan",
+        fontWeight: "500",
+        width: 0.7 * Dimensions.get("window").width,
+        textAlign: "left",
+        maxHeight: 30
+    },
+
+    formInput: {
+        width: 0.7 * Dimensions.get("window").width,
+        maxHeight: 30
     },
 
     errMsg: {
         fontSize: 15,
-        color: "red"
+        color: "red",
+        maxHeight: 30
     }
 });
--- containers/SignupPage.js
+++ containers/SignupPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'React';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Dimensions } from 'react-native';
 import { firebase, firestore } from '../utils/firebase';
 import { Constants } from 'expo';
 
@@ -16,12 +16,14 @@ export default class LoginPage extends Component {
             confirmPassword: "",
             errMsg: ""
         };
+
+        this.trySignup = this.trySignup.bind(this);
     };
 
     trySignup() {
-        let emailAddress, password = this.state;
+        let emailAddress, password, confirmPassword = this.state;
 
-        if (password !== this.state.confirmPassword) {
+        if (password !== confirmPassword) {
             this.setState({errMsg: "Passwords do not match"});
             return;
         } else if (this.state.firstName === "" || this.state.lastName === "" || this.state.username === "" || emailAddress === "" || password === "" || this.state.confirmPassword === "") {
@@ -42,57 +44,71 @@ export default class LoginPage extends Component {
             });
     };
 
-    handleInputChange(inputType, value) {
-        this.setState({[inputType]: value})
-    };
-
     render() {
         return (
             <View style={styles.signupPage}>
-                <Text>please dont do cocaine ey cocaine ruin your brain ey</Text>
+                <Text style={styles.logo}>Babysitter</Text>
 
                 <Text style={styles.inputLabel}>First Name</Text>
-                <TextInput textContentType="givenName" value={this.state.firstName} onChange={event => {this.handleInputChange("firstName", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="givenName" value={this.state.firstName} onChangeText={text => this.setState({firstName: text})} />
 
                 <Text style={styles.inputLabel}>Last Name</Text>
-                <TextInput textContentType="familyName" value={this.state.lastName} onChange={event => {this.handleInputChange("lastName", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="familyName" value={this.state.lastName} onChangeText={text => this.setState({lastName: text})} />
 
                 <Text style={styles.inputLabel}>Username</Text>
-                <TextInput textContentType="username" value={this.state.username} onChange={event => {this.handleInputChange("username", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="username" value={this.state.username} onChangeText={text => this.setState({username: text})} />
 
                 <Text style={styles.inputLabel}>Email Address</Text>
-                <TextInput textContentType="emailAddress" value={this.state.emailAddress} onChange={event => {this.handleInputChange("emailAddress", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="emailAddress" value={this.state.emailAddress} onChangeText={text => this.setState({emailAddress: text})} />
 
                 <Text style={styles.inputLabel}>Password</Text>
-                <TextInput textContentType="password" secureTextEntry={true} value={this.state.password} onChange={event => {this.handleInputChange("password", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="password" secureTextEntry={true} value={this.state.password} onChangeText={text => this.setState({password: text})} />
 
                 <Text style={styles.inputLabel}>Confirm Password</Text>
-                <TextInput textContentType="none" secureTextEntry={true} value={this.state.confirmPassword} onChange={event => {this.handleInputChange("confirmPassword", event.target.value)}} />
+                <TextInput style={styles.formInput} textContentType="password" secureTextEntry={true} value={this.state.confirmPassword} onChangeText={text => this.setState({confirmPassword: text})} />
 
                 <Text style={styles.errMsg}>{this.state.errMsg}</Text>
 
-                <Button title="Sign Up" onPress={this.trySignup.bind(this)} />
+                <Button title="Sign Up" onPress={this.trySignup} />
             </View>
         );
     };
 };
 
 const styles = StyleSheet.create({
+    logo: {
+        marginTop: 30,
+        marginBottom: 30,
+        fontSize: 70,
+        fontWeight: "700",
+        color: "cornflowerblue"
+    },
+
     signupPage: {
         flex: 1,
         justifyContent: "space-around",
-        alignItems: "baseline",
+        alignItems: "center",
         marginTop: Constants.statusBarHeight,
         backgroundColor: "lightblue"
     },
 
     inputLabel: {
         fontSize: 20,
-        color: "gray"
+        color: "darkcyan",
+        fontWeight: "500",
+        width: 0.7 * Dimensions.get("window").width,
+        textAlign: "left",
+        maxHeight: 30
+    },
+
+    formInput: {
+        width: 0.7 * Dimensions.get("window").width,
+        maxHeight: 30
     },
 
     errMsg: {
         fontSize: 15,
-        color: "red"
+        color: "red",
+        maxHeight: 30
     }
 });
